6178e59f40522f1cafe7c663788a535feaf4d1dd
natsukashiixo <jesper.varg.viking@gmail.com>
2025-10-24 04:00:01 +0200

    Auto-sync: 2025-10-24 04:00:01

--- scripts/smartcopypaste.sh
+++ scripts/smartcopypaste.sh
@@ -1,14 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-key=$1
-terminal_class=$2
+action=$1
+term_class=$2
+active_window="$(hyprctl activewindow -j | jq -r '.class')"
 
-active_class=$(hyprctl activewindow -j | jq -r '.class')
+case "$action" in
+  copy)
+    key='C'
+    ;;
+  paste)
+    key='V'
+    ;;
+  cut)
+    key='X'
+    ;;
+  *)
+  echo "$action not recognized, perhaps a typo?" >&2
+  exit 1
+  ;;
+esac
 
-if [ "$active_class" = "$terminal_class" ]; then
-    shortcut="CTRL_SHIFT,$key,"
+if [[ $active_window == *"$term_class"* ]]; then
+  shortcut="CTRL_SHIFT,$key,"
 else
-    shortcut="CTRL,$key,"
+  shortcut="CTRL,$key,"
 fi
 
 hyprctl dispatch sendshortcut "$shortcut"
